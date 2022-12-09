b78090e7011fd2eaaba80d64dbdd897c9c92e382
Liquan Yang <liquan.yang@torqata.com>
2022-12-09 10:20:07 -0500

    complete day09 solution

--- day09/solver.py
+++ day09/solver.py
@@ -1,116 +1,46 @@
+from math import sqrt
 from pathlib import Path
 
 
 def update_position(lead, lag):
-    lead_x, lead_y = lead
-    lag_x, lag_y = lag
+    distance = sqrt((lead["x"] - lag["x"]) ** 2 + (lead["y"] - lag["y"]) ** 2)
 
-    if lag_x - lead_x > 1:
-        lag_x -= 1
-        lag_y -= lag_y - lead_y
-    elif lead_x - lag_x > 1:
-        lag_x += 1
-        lag_y -= lag_y - lead_y
-    elif lag_y - lead_y > 1:
-        lag_y -= 1
-        lag_x -= lag_x - lead_x
-    elif lead_y - lag_y > 1:
-        lag_y += 1
-        lag_x -= lag_x - lead_x
+    if distance >= 2:
+        if lag["x"] != lead["x"]:
+            lag["x"] += 1 if lead["x"] > lag["x"] else -1
+        if lag["y"] != lead["y"]:
+            lag["y"] += 1 if lead["y"] > lag["y"] else -1
 
-    return lag_x, lag_y
+    return lag
 
 
-def count_visited_positions(filepath):
+def update_direction(knot, direction):
+    if direction == "U":
+        return {"x": knot["x"], "y": knot["y"] - 1}
+    if direction == "D":
+        return {"x": knot["x"], "y": knot["y"] + 1}
+    if direction == "L":
+        return {"x": knot["x"] - 1, "y": knot["y"]}
+    return {"x": knot["x"] + 1, "y": knot["y"]}
+
+
+def count_visited_positions(filepath, knot_size):
     visited = {(0, 0)}
-    H_X, H_Y = T_X, T_Y = (0, 0)
+    knots = [{"x": 0, "y": 0} for _ in range(knot_size)]
 
     with Path(filepath).open("r") as f:
         for line in f:
             values = line.strip()
             direction, times = values.split()
 
-            if direction == "U":
-                for _ in range(int(times)):
-                    H_Y -= 1
-                    T_X, T_Y = update_position((H_X, H_Y), (T_X, T_Y))
-                    visited.add((T_X, T_Y))
-            elif direction == "D":
-                for _ in range(int(times)):
-                    H_Y += 1
-                    T_X, T_Y = update_position((H_X, H_Y), (T_X, T_Y))
-                    visited.add((T_X, T_Y))
-            elif direction == "L":
-                for _ in range(int(times)):
-                    H_X -= 1
-                    T_X, T_Y = update_position((H_X, H_Y), (T_X, T_Y))
-                    visited.add((T_X, T_Y))
-            else:
-                for _ in range(int(times)):
-                    H_X += 1
-                    T_X, T_Y = update_position((H_X, H_Y), (T_X, T_Y))
-                    visited.add((T_X, T_Y))
+            for _ in range(int(times)):
+                for i in range(knot_size):
+                    if i == 0:
+                        knots[i] = update_direction(knots[i], direction)
+                    else:
+                        knots[i] = update_position(knots[i - 1], knots[i])
 
-    return len(visited)
-
-
-def count_visited_positions2(filepath):
-    visited = {(0, 0)}
-    knots = dict([(i, {"x": 0, "y": 0}) for i in range(10)])
-
-    with Path(filepath).open("r") as f:
-        for line in f:
-            values = line.strip()
-            direction, times = values.split()
-
-            if direction == "U":
-                for _ in range(int(times)):
-                    knots[0]["y"] -= 1
-
-                    for i in range(1, 10):
-                        knots[i]["x"], knots[i]["y"] = update_position(
-                            (knots[i - 1]["x"], knots[i - 1]["y"]),
-                            (knots[i]["x"], knots[i]["y"]),
-                        )
-
-                        if i == 9:
-                            visited.add((knots[i]["x"], knots[i]["y"]))
-            elif direction == "D":
-                for _ in range(int(times)):
-                    knots[0]["y"] += 1
-
-                    for i in range(1, 10):
-                        knots[i]["x"], knots[i]["y"] = update_position(
-                            (knots[i - 1]["x"], knots[i - 1]["y"]),
-                            (knots[i]["x"], knots[i]["y"]),
-                        )
-
-                        if i == 9:
-                            visited.add((knots[i]["x"], knots[i]["y"]))
-            elif direction == "L":
-                for _ in range(int(times)):
-                    knots[0]["x"] -= 1
-
-                    for i in range(1, 10):
-                        knots[i]["x"], knots[i]["y"] = update_position(
-                            (knots[i - 1]["x"], knots[i - 1]["y"]),
-                            (knots[i]["x"], knots[i]["y"]),
-                        )
-
-                        if i == 9:
-                            visited.add((knots[i]["x"], knots[i]["y"]))
-            else:
-                for _ in range(int(times)):
-                    knots[0]["x"] += 1
-
-                    for i in range(1, 10):
-                        knots[i]["x"], knots[i]["y"] = update_position(
-                            (knots[i - 1]["x"], knots[i - 1]["y"]),
-                            (knots[i]["x"], knots[i]["y"]),
-                        )
-
-                        if i == 9:
-                            visited.add((knots[i]["x"], knots[i]["y"]))
+                visited.add((knots[-1]["x"], knots[-1]["y"]))
 
     return len(visited)
 
@@ -131,6 +61,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.part == "1":
-        print(count_visited_positions(args.input_path))
+        print(count_visited_positions(args.input_path, 2))
     else:
-        print(count_visited_positions2(args.input_path))
+        print(count_visited_positions(args.input_path, 10))
