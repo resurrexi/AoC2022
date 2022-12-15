1830e4577365a8ae9aa12bfb0e314891108f32b0
Liquan Yang <liquan.yang@torqata.com>
2022-12-15 14:01:27 -0500

    wip: use multithreading for part2 for day15

--- day15/solver.py
+++ day15/solver.py
@@ -1,5 +1,8 @@
+import functools
 import re
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+from threading import Event
 
 
 def get_bounds(devices):
@@ -55,11 +58,63 @@ def get_coverage(devices, line):
         # is this point on the line within any coverage of a S
         for k, v in sensors.items():
             distance = get_distance((line, col), k)
+
             if distance <= v["coverage"]:
                 coverage += 1
                 break
 
     return coverage
+
+
+def parallel_search(position, devices, distance_fn, event):
+    # another task found beacon; terminate
+    if event.is_set():
+        return False
+
+    sensors = dict([(k, v) for k, v in devices.items() if v["type"] == "S"])
+    row, col = position
+
+    if (row, col) in devices:
+        return False
+
+    for k, v in sensors.items():
+        distance = distance_fn((row, col), k)
+
+        if distance <= v["coverage"]:
+            return False
+
+    # exhausted all sensors, this position is the beacon
+    return True
+
+
+def search_beacon(devices, search_space):
+    min_row, max_row, min_col, max_col = search_space
+    event = Event()
+
+    # feed them to worker pool
+    with ThreadPoolExecutor(max_workers=1000) as executor:
+        for row in range(min_row, max_row + 1):
+            # generate list of candidates to search
+            candidates = [(row, col) for col in range(min_col, max_col + 1)]
+
+            results = executor.map(
+                functools.partial(
+                    parallel_search,
+                    devices=devices,
+                    distance_fn=get_distance,
+                    event=event,
+                ),
+                candidates,
+            )
+
+            for pos, found in zip(candidates, results):
+                if found:
+                    # mark event to set so remaining tasks finish early
+                    event.set()
+                    return pos
+
+    # return OOB coords if beacon not found
+    return min_row - 1, min_col - 1
 
 
 if __name__ == "__main__":
@@ -82,4 +137,5 @@ if __name__ == "__main__":
     if args.part == "1":
         print(get_coverage(devices, 2000000))
     else:
-        pass
+        position = search_beacon(devices, (0, 4000000, 0, 4000000))
+        print(position[1] * 4000000 + position[0])
